1ef6371a98ea93a49ac9a4b383d9283dabd369cc
Pedro Tomaz <102549864+PedroHTomaz@users.noreply.github.com>
2022-09-28 12:45:15 -0300

    otimizacao front

--- script/submit.php
+++ script/submit.php
@@ -7,7 +7,7 @@ $module = $_POST['module'];
 $data_envio = date('d/m/Y');
 $hora_envio = date('H:i:s');
 
-// Compo E-mail
+// Corpo E-mail
 $arquivo = "
 <style type='text/css'>
 body {
@@ -49,23 +49,23 @@ text-decoration: none;
 
 //enviar
 
-  // emails para quem será enviado o formulário
-  //$destino = "fra.wellington.oliveira@gmail.com";
-  $destino = "erick.barreto@trt7.jus.br";
-  $assunto = "Teste formulario criação card jira";
+//emails para quem será enviado o formulário
+//$destino = "fra.wellington.oliveira@gmail.com";
+$destino = "erick.barreto@trt7.jus.br";
+$assunto = "Teste formulario criação card jira";
 
-  // É necessário indicar que o formato do e-mail é html
-  $headers  = 'MIME-Version: 1.0' . "\r\n";
-      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-      $headers .= 'From: $nome <$email>';
-  //$headers .= "Bcc: $EmailPadrao\r\n";
+// É necessário indicar que o formato do e-mail é html
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: $nome <$email>';
+//$headers .= "Bcc: $EmailPadrao\r\n";
 
-  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
-  if($enviaremail){
-  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
-  echo "<meta http-equiv='refresh' content='10;URL=contato.php'>";
-  } else {
-  $mgm = "ERRO AO ENVIAR E-MAIL!";
-  echo "";
-  }
+$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+if($enviaremail){
+$mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+echo "<meta http-equiv='refresh' content='10;URL=contato.php'>";
+} else {
+$mgm = "ERRO AO ENVIAR E-MAIL!";
+echo "";
+}
 ?>
